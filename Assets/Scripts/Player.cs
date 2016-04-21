b2ce0d8550b6fbd4156e91af41f5023ed6db6ff2
Max Gordon <gordonmaxc@gmail.com>
2016-04-21 12:54:34 -0700

    Added resource assets and smoothed out movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,15 +5,21 @@ public class Player : MonoBehaviour
 {
 
 	public Vector2 Destination { get; set; }
+	public LayerMask blockingLayer;
 
+	private BoxCollider2D boxCollider;
+	private Rigidbody2D rb2D;
+	private float inverseMoveTime;
 
-	private float moveSpeed = 3f;
+	private float moveSpeed = 0.01f;
 //	private float pickupRange = 0.5f;
 //	private Animator animator;
 
 	void Start ()
 	{
 //		animator = GetComponent<Animator> ();
+		boxCollider = GetComponent<BoxCollider2D>();
+		rb2D = GetComponent<Rigidbody2D> ();
 	}
 
 	void Update ()
@@ -34,10 +40,17 @@ public class Player : MonoBehaviour
 
 	void MoveToDestination ()
 	{
-		
-		transform.position = Vector2.MoveTowards (transform.position, Destination, moveSpeed * Time.deltaTime);
-
+			StartCoroutine(SmoothMovement(Destination));
 	}
 
+	IEnumerator SmoothMovement(Vector2 end)
+	{
+		float sqrRemainingDistance = ((Vector2) transform.position - end).sqrMagnitude;
 
+		while(sqrRemainingDistance > float.Epsilon)
+		{
+			transform.position = Vector2.MoveTowards (transform.position, Destination, moveSpeed * Time.deltaTime);
+			yield return null;
+		}
+	}
 }
---
+++ Assets/Scripts/Resource.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resource : MonoBehaviour
+{
+//	public AudioClip chopSound1;
+	//1 of 2 audio clips that play when the wall is attacked by the player.
+//	public AudioClip chopSound2;
+	//2 of 2 audio clips that play when the wall is attacked by the player.
+	//Alternate sprite to display after Wall has been attacked by player.
+	public Sprite[] sprites;
+
+	private int hp;
+	private int damage = 0;
+	private SpriteRenderer spriteRenderer;
+
+	void Awake ()
+	{
+		hp = sprites.Length;
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		spriteRenderer.sprite = sprites[damage];
+	}
+
+
+	//DamageWall is called when the player attacks a wall.
+	public void TakeDamage (int loss)
+	{
+		//Call the RandomizeSfx function of SoundManager to play one of two chop sounds.
+//		SoundManager.instance.RandomizeSfx (chopSound1, chopSound2);
+
+		damage += loss;
+		spriteRenderer.sprite = sprites[damage];
+
+		if (damage >= hp){
+			gameObject.SetActive (false);
+		}
+	}
+}
+
