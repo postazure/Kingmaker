b7a18c738abdce9835c4e97a1ecf1c718b3c3f99
Max Gordon <gordonmaxc@gmail.com>
2016-04-22 08:21:02 -0700

    Dont try to move if your alread at the target or destination

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -28,19 +28,25 @@ public class Player : MonoBehaviour
 
 	void Update ()
 	{
+
 		SetDestination ();
 
-		if (isTargetInRange () && Input.GetMouseButtonDown (0)) {
-			if (Target is Resource) {
-				animator.SetTrigger ("playerChop");
-				((Resource)Target).ApplyDamage (1);
-			}
-		}
-	}
+//		if (isTargetInRange () && Input.GetMouseButtonDown (0)) {
+//			if (Target is Resource) {
+//				animator.SetTrigger ("playerChop");
+//				((Resource)Target).ApplyDamage (1);
+//			}
+//		}
 
-	void FixedUpdate ()
-	{
-		Move (Destination);
+		if (Target && isObjectInRange(Target.transform.position, meleeRange)) {
+			if (Input.GetMouseButtonDown (0)) {
+				if (Target is Resource) {
+					HarvestResource ();
+				}
+			}
+		} else {
+			Move (Destination);
+		}
 	}
 
 	void SetDestination ()
@@ -51,24 +57,27 @@ public class Player : MonoBehaviour
 		}
 	}
 
-	//	void OnCollisionEnter2D(Collision2D coll){
-
-	//	}	
+		void OnCollisionEnter2D(Collision2D coll){
+		rb2D.velocity = new Vector2 () * 0;
+		}	
 		
 	void Move (Vector2 targetPos)
 	{
-		if ((targetPos - (Vector2)transform.position).sqrMagnitude < 0.5) {
+		if (isObjectInRange (targetPos)) {
 			rb2D.velocity = new Vector2 () * 0;
 		} else {
 			rb2D.velocity = ((targetPos - (Vector2)transform.position)).normalized * moveSpeed;
 		}
 	}
-
-	bool isTargetInRange ()
+		
+	bool isObjectInRange (Vector3 objectsPostion, float addDistance = 0)
 	{
-		if (Target == null) {
-			return false;
-		}
-		return (transform.position - Target.transform.position).sqrMagnitude < meleeRange;
+		return (transform.position - objectsPostion).sqrMagnitude < 0.5 + addDistance;
+	}
+
+	void HarvestResource ()
+	{
+		animator.SetTrigger ("playerChop");
+		((Resource)Target).ApplyDamage (1);
 	}
 }
